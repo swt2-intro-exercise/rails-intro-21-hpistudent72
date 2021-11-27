50015c9e84f7c03f06288dc0bd421bb9eb3864c2
hpistudent72 <florian.krummrey@student.hpi.uni-potsdam.de>
2021-11-27 17:13:50 +0100

    12 completed

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -3,4 +3,11 @@ describe "Edit Author Page", type:  :feature do
         FactoryBot.create :author
         visit edit_author_path(1)
     end
+
+    it "should be able to change First Name without error" do
+        FactoryBot.create :author
+        visit edit_author_path(1)
+        page.fill_in 'author[first_name]', with: 'Bob'
+        find('input[type="submit"]').click
+    end
 end
