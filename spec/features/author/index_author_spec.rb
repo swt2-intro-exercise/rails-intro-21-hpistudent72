aa6958d2d78391172fbe0520a5a284cc565fb90b
hpistudent72 <florian.krummrey@student.hpi.uni-potsdam.de>
2021-11-27 16:10:01 +0100

    8 completed

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,4 +2,9 @@ describe "Index Author Page", type:  :feature do
     it "should exist" do
         visit authors_path
     end
+
+    it "should have link to new author form" do
+        visit authors_path
+        expect(page).to have_link 'New Author', href: new_author_path
+    end
 end
