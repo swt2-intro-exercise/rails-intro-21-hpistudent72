2e9e65454960062ba4d31288861600817a934fdd
hpistudent72 <florian.krummrey@student.hpi.uni-potsdam.de>
2021-11-27 14:48:27 +0100

    Was fehlt?

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,9 +1,9 @@
 describe "Author Model", type: :model do
-    it "created author should be Alan Turing, http://wikipedia.de/Alan_Turing" do
-        author = Author.new(first_name:'Alan', last_name:'Turing', homepage: 'http://wikipedia.de/Alan_Turing')
-        expect(author.first_name).to eq('Alan')
-        expect(author.last_name).to eq('Turing')
-        expect(author.homepage).to eq('http://wikipedia.de/Alan_Turing')
-        expect(author.name).to eq('Alan Turing')
-    end
+  it "should have Alan Turing, http://wikipedia.de/Alan_Turing" do
+      author = Author.new(first_name:'Alan', last_name:'Turing', homepage: 'http://wikipedia.de/Alan_Turing')
+      expect(author.first_name).to eq('Alan')
+      expect(author.last_name).to eq('Turing')
+      expect(author.homepage).to eq('http://wikipedia.de/Alan_Turing')
+      expect(author.name).to eq('Alan Turing')
+  end
 end
